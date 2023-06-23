48658f55837915cc86893afb3a4e54b01f8cb55c
Coen Dye <128545261+Coen-Dye@users.noreply.github.com>
2023-06-23 14:21:55 +1200

    Update menu_rps.py
    Added comments to explain what is happening throughout the code

--- menu_rps.py
+++ menu_rps.py
@@ -30,25 +30,25 @@ def menu(): #Menu function
             print("Invalid input, enter a valid integer.")
 
 
-def get_user_choice(username):
+def get_user_choice(username):  # Function to get the user's choice for Game 1
     valid_choices = ['rock', 'paper', 'scissors']
-    flag1 = True
+    flag1 = True # flag1 is the flag for this function used to create a loop
     while flag1:
         try:
             user_choice = str(input(f"Please enter your choice {username} (rock/paper/scissors): ").lower())
             if user_choice not in valid_choices:
-                raise ValueError
+                raise ValueError #Raises the value error and outputs the error message
             return user_choice
         except ValueError:
             print("Invalid choice. Please enter rock, paper, or scissors.")
 
-def get_computer_choice():
-    return random.choice(['rock', 'paper', 'scissors'])
+def get_computer_choice(): #Function to get the computer's choice for future use
+    return random.choice(['rock', 'paper', 'scissors']) #Uses a list and random.choice to choose a random string within the list
 
-def determine_winner(user_choice, computer_choice):
+def determine_winner(user_choice, computer_choice): #Function that uses calculations to determine the winner
     if user_choice == computer_choice:
         return "draw"
-    elif (
+    elif ( #Block of code containing all possible winning combinations for the user
         (user_choice == 'rock' and computer_choice == 'scissors') or
         (user_choice == 'paper' and computer_choice == 'rock') or
         (user_choice == 'scissors' and computer_choice == 'paper')
@@ -57,7 +57,7 @@ def determine_winner(user_choice, computer_choice):
     else:
         return "computer"
 
-def play_again(username):
+def play_again(username): #Play again function that asks the user whether they would like to continue or return to the menu
     flag2 = True
     while flag2:
         try:
@@ -72,16 +72,16 @@ def play_again(username):
 def play_rps(username):
     print(f"\nWelcome to Survival Rock Paper Scissors {username}!")
     wins = 0
-    continue_playing = True
+    continue_playing = True #Main boolean variable that creates the overall loop for the program
     while continue_playing:
-        user_choice = get_user_choice(username)
-        computer_choice = get_computer_choice()
+        user_choice = get_user_choice(username) #Calls the function to get the users choice
+        computer_choice = get_computer_choice() #Calls the function to get the computers choice
 
-        winner = determine_winner(user_choice, computer_choice)
+        winner = determine_winner(user_choice, computer_choice) #Calls the function that calculates the winner based on the previous inputs
 
         print(f"\nYou chose: {user_choice}")
         print(f"The computer chose: {computer_choice}")
-        
+        #Game result outputs
         if winner == "user":
             wins += 1
             print("\nCongratulations! You win!")
@@ -91,13 +91,13 @@ def play_rps(username):
         else:
             print("\nIt's a draw! Let's keep playing.")
 
-    print(f"Total wins: {wins}")
+    print(f"Total wins: {wins}") #Displays total wins that the user has accumulated in that round
 
-    play_again_input = play_again(username)
+    play_again_input = play_again(username) #Calls the play again function after the user loses to ask if they wish to play again or return to menu
 
     if play_again_input == 'yes':
         play_rps(username)
     else:
-        print(f"\nThanks for playing {username}! Returning to menu...")
+        print(f"\nThanks for playing {username}! Returning to menu...") #This will output if they decide not to play again and the user will be returned to the menu
 
 menu()
