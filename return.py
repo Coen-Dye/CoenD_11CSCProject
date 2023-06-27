2032ef3fffa8ce1f12db8a4155b7d26a59f67eec
Coen Dye <128545261+Coen-Dye@users.noreply.github.com>
2023-06-28 10:21:23 +1200

    Delete return.py

--- return.py
+++
@@ -1,9 +0,0 @@
-def game_function(username):
-    # Use the 'username' variable in the function
-    print(f"Hello, {username}! This is the game function.")
-
-# Call the 'menu()' function and store the returned username
-username = menu()
-
-# Call the 'game_function()' and pass the 'username' as an argument
-game_function(username)
